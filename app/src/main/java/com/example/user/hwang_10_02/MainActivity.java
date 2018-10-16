39638324309d9d09ec22cd225634cd503dfc5bba
HwnagInWoo <allhpy35@gmail.com>
2018-10-16 16:55:46 +0900

    RecordVoice

--- app/src/main/java/com/example/user/hwang_10_02/MainActivity.java
+++ app/src/main/java/com/example/user/hwang_10_02/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.user.hwang_10_02;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.ViewUtils;
@@ -9,10 +10,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-    protected Button btnHomepage, btnDial, btnCall, SMS, Map, Voice;
-    TextView TextView;
+import java.util.Locale;
 
+public class MainActivity extends AppCompatActivity {
+    protected Button btnHomepage, btnDial, btnCall, SMS, Map, Voice, Gps, Record;
+    TextView TextView, VoiceRecord;
+        private int CODE_RECOG = 1234;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -65,14 +68,25 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
         TextView = (TextView)findViewById(R.id.TextView);
 
-        Voice=(Button)findViewById(R.id.Voice);
-        Voice.setOnClickListener(new View.OnClickListener() {
+        VoiceRecord = (TextView)findViewById(R.id.VoiceRecord);
+        Record=(Button)findViewById(R.id.Record);
+        Record.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                    voiceRecog();
             }
         });
+
+
+        }
+    private void voiceRecog() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);      //스마트폰에있는걸 이용해서 좀더 정확한 음성인식을 하기 위해 설정하였다
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.KOREAN);
+        startActivityForResult(intent, CODE_RECOG);
+
     }
 }

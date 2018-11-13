8b4e6ac03903c58ef1f54de07ea193a14abcb52a
HwnagInWoo <allhpy35@gmail.com>
2018-11-13 15:58:44 +0900

    11_13_BitMap_3

--- app/src/main/java/com/example/user/hwang_10_02/BitMapRunable.java
+++ app/src/main/java/com/example/user/hwang_10_02/BitMapRunable.java
@@ -1,20 +1,37 @@
 package com.example.user.hwang_10_02;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.widget.ImageView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 
 public class BitMapRunable implements Runnable {
 
-    protected ImageView ImageBitMap;
+    protected ImageView IvBitMap;
     protected  String sBitmapUrI;
 
 
-    public BitMapRunable(ImageView BitMap, String sBitmapUrI) {
-            this.ImageBitMap = BitMap;
+    public BitMapRunable(ImageView IvBitMap, String sBitmapUrI) {
+            this.IvBitMap = IvBitMap;
             this.sBitmapUrI = sBitmapUrI;
         }
 
     @Override
     public void run() {
+        try {
+            final Bitmap bitmap = BitmapFactory.decodeStream((InputStream)(new URL(sBitmapUrI).getContent()));
+            IvBitMap.post(new Runnable() {
+                @Override
+                public void run() {
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
 
     }
 }
